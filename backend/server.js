d4483e888860c2d46e7f5689da4d16efcc64d8af
AnjaliK-123 <anjalikhadake888@gmail.com>
2025-01-05 15:22:56 -0800

    Fixed incorrect spelling for mongoose

--- backend/server.js
+++ backend/server.js
@@ -1,23 +1,24 @@
+// load env variables from .env file
+require('dotenv').config();
 // setup express module 
 const express = require('express');
-// setup moongoose module 
-const moongoose = require('moongose');
+// setup mongoose module 
+const mongoose = require('mongoose');
 // setup middleware 
 const cors = require('cors');
 // instance of express 
 const app = express(); 
-// instace of middleware 
+// instance of middleware 
 app.use(cors());
 app.use(express.json());
 // database connection 
-moongoose.connect(
+mongoose.connect(
     process.env.MONGODB_URI, {
-    usenewUrlParser: true, useUnifiedTopology: true
+    useNewUrlParser: true, useUnifiedTopology: true
 })
     .then(() => console.log('MongoDB connected'))
     .catch(err => console.log(err));
 
-//listen on port
-const PORT = process.env.PORT || 5000;
-app.listen(PORT, () => console.log('Server running on port ${PORT}'));
-
+// listen on port
+const PORT = process.env.PORT || 5001;
+app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
