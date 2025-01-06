c47a32b943bc610125b17f102feb04f8e2f218a6
AnjaliK-123 <anjalikhadake888@gmail.com>
2025-01-05 19:21:46 -0800

    Added budget route

---
+++ backend/routes/budget.js
@@ -0,0 +1,51 @@
+// import express
+const express = require('express');
+// import jwt
+const jwt = require('jsonwebtoken');
+// import router
+const router = express.Router();
+// import budget model
+const budget = require('../models/budgetEntry');
+
+// Middleware to authenticate token
+const authenticateToken = (res, req, next) => {
+    //get auth header value
+    const token = req.headers['authorization']?.split(' ')[1];
+    // if token invalid
+    if (!token) {
+        // uauthorized
+        return res.sendStatus(401);
+    }
+    // verify token
+    jwt.verify(token, proces.env.JWT_SECRET, (err, user) => {
+        if (err) {
+            // forbidden
+            return res.sendStatus(403);
+        }
+        // assign decode user to user property of request body  
+        req.user = user;
+        // proceed to next middleware
+        next();
+    });
+}
+
+// Add Budget Entry
+rotuer.post('/', authenticateToken, async (req, res) => {
+    // get budget details
+    const { amount, category, description } = req.body;
+    // create instance of budget model
+    const newEntry = new budegt({
+        userId: req.user.id,
+        amount,
+        category,
+        description
+    });
+    // save entry
+    await enwEntry.save();
+    // send response with new entry
+    res.status(201).json(newEntry);
+}
+    );
+// Get Budget Entries
+// Delete Budget Entry
+// export router 
