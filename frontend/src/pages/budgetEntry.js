948577ad2132702dc05f64437cd4c5d55dbc861d
AnjaliK-123 <anjalikhadake888@gmail.com>
2025-01-11 19:31:51 -0800

    Added axios and handling error and success

--- frontend/src/pages/budgetEntry.js
+++ frontend/src/pages/budgetEntry.js
@@ -1,14 +1,33 @@
 import React, { useState } from 'react';
+import axios from 'axios';
 
 const BudgetEntry = () => {
     // state variables
     const [amount, setAmount] = useState('');
     const [category, setCategory] = useState('');
     const [description, setDescription] = useState('');
+    const [error, setError] = useState('');
+    const [success, setSuccess] = useState('');
 
-    const handleSubmit = (e) => {
+    const handleSubmit = async (e) => {
         e.preventDefault();
-        // handle submission        
+        // handle submission 
+        try {
+            // used axios as middleware to post data to backend
+            const response = await axios.post('http://localhost:5001/api/budget', {
+                amount,
+                category,
+                description
+            });
+            // handle success
+            setSuccess(response.data);
+            setError(null);
+
+        } catch (error) {
+            // handle error
+            setError(error.response?.data || 'Entry failed');
+            setSuccess(null);
+        }
     }
 
     return (
@@ -17,6 +36,7 @@ const BudgetEntry = () => {
             <form onSubmit={handleSubmit}>
                 <input type="text" placeholder="Amount" value={amount} onChange={(e) => setAmount(e.target.value)}  required/>
                 <input type="text" placeholder="Category" value={category} onChange={(e) => setCategory(e.target.value)} required />
+                <input type="text" placeholder="Description" value={description} onChange={(e) => setDescription(e.target.value)} required />
                 <button type="submit">Add Entry</button>
             </form>       
         </div>
